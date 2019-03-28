dee3be743654915e21cd5a61b28e609178e32d67
Triston Armstrong <triston95strong@gmail.com>
2019-03-27 19:35:26 -0500

    added animated navbar

--- main.js
+++ main.js
@@ -1,17 +1,21 @@
 window.onscroll = function() {navScroll()};
 
 const navScroll = () => {
-    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-        document.querySelector("#navbar").style.padding = "20px 5px";
-        document.querySelector("#navbar").style.boxShadow = "0px 8px 5px -2px darkgrey";
-        document.querySelectorAll(".s").forEach(x => x.style.fontSize = ".5rem")
-        document.getElementById("navbar").style.position = "fixed";
-    }else{
-        document.querySelector("#navbar").style.padding = "40px 10px";
-        document.querySelector("#navbar").style.borderBottom = "none";
-        document.querySelector("#navbar").style.boxShadow = "none";
-        document.querySelectorAll(".s").forEach(x => x.style.fontSize = "1.2rem")
-        document.getElementById("navbar").style.position = "relative";
-
+    var navbar = document.querySelector("#navbar").style;
+    if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {//when scrolled
+        navbar.padding = "5px 5px";
+        navbar.boxShadow = "0px 8px 5px -2px darkgrey";
+        navbar.position = "fixed";
+        navbar.left = "11.5%";
+        navbar.transition = "none";
+        document.querySelectorAll(".s").forEach(x => x.style.fontSize = "0.8rem");
+    }else{//when at top
+        navbar.padding = "40px 10px";
+        navbar.borderBottom = "none";
+        navbar.boxShadow = "none";
+        navbar.position = "relative";
+        navbar.left = "11.5%";
+        navbar.transition = "0.2s ease-in";
+        document.querySelectorAll(".s").forEach(x => x.style.fontSize = "1.2rem");
     }
 }
